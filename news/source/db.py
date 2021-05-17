789cbeaa48f48cf2557b153ed683ecf47c59424c
Elena Gordeeva <gordeewa29@gmail.com>
2021-05-17 17:53:32 +0300

    change variables' names

--- news/source/db.py
+++ news/source/db.py
@@ -14,21 +14,21 @@ Base = declarative_base()
 
 class TINews(Base):
     __tablename__ = "ti_news"
-    ti_news_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     time = Column(String(30), default=None)
     title = Column(String, default=None)
     text = Column(String, default=None)
     match = relationship("CompetitorsNews", backref=backref("matching_news"))
 
     def __repr__(self):
-        return f"{self.ti_news_id} | {self.time} | {self.title}"
+        return f"{self.id} | {self.time} | {self.title}"
 
 
 class CompetitorsNews(Base):
     __tablename__ = "competitors_news"
-    comp_news_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     is_match = Column(Integer, default=None)
     matching_news_id = Column(Integer, ForeignKey("ti_news.ti_news_id"), default=None)
 
     def __repr__(self):
-        return f"{self.comp_news_id} | is match: {self.is_match} | matching news: {self.matching_news.title}"
+        return f"{self.id} | is match: {self.is_match} | matching news: {self.matching_news.title}"
